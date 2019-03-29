c0773968af29e2f3434a12e5df310439da8262cc
erikvw <ew2789@gmail.com>
2019-03-29 18:33:41 -0500

    setup

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
-    install_requires=["edc-base"],
+    install_requires=["edc-base", "edc_model"],
     python_requires=">=3.7",
     tests_require=tests_require,
     test_suite='runtests.main',

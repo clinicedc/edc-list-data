a8bc86762ed0b36701b90585ff7c37e7f53e8747
erikvw <ew2789@gmail.com>
2019-04-10 00:01:31 -0500

    black

--- edc_list_data/management/commands/load_list_data.py
+++ edc_list_data/management/commands/load_list_data.py
@@ -5,8 +5,8 @@ from edc_list_data.site_list_data import site_list_data, SiteListDataError
 
 class Command(BaseCommand):
 
-    help = 'Populates list data and other static model data from list_data.py'
-    module_name = 'list_data'
+    help = "Populates list data and other static model data from list_data.py"
+    module_name = "list_data"
 
     def handle(self, *args, **options):
         try:
--- edc_list_data/site_list_data.py
+++ edc_list_data/site_list_data.py
@@ -20,22 +20,21 @@ class SiteListData:
     """
 
     def autodiscover(self, module_name=None, verbose=True):
-        if 'migrate' not in sys.argv and 'makemigrations' not in sys.argv:
-            module_name = module_name or 'list_data'
+        if "migrate" not in sys.argv and "makemigrations" not in sys.argv:
+            module_name = module_name or "list_data"
             writer = sys.stdout.write if verbose else lambda x: x
             style = color_style()
-            writer(f' * checking for site {module_name} ...\n')
+            writer(f" * checking for site {module_name} ...\n")
             for app in django_apps.app_configs:
-                writer(f' * searching {app}           \r')
+                writer(f" * searching {app}           \r")
                 try:
                     mod = import_module(app)
                     try:
-                        import_module(f'{app}.{module_name}')
-                        writer(
-                            f' * loading \'{module_name}\' from \'{app}\'\n')
+                        import_module(f"{app}.{module_name}")
+                        writer(f" * loading '{module_name}' from '{app}'\n")
                     except PreloadDataError as e:
-                        writer(f'   - loading {app}.{module_name} ... ')
-                        writer(style.ERROR(f'ERROR! {e}\n'))
+                        writer(f"   - loading {app}.{module_name} ... ")
+                        writer(style.ERROR(f"ERROR! {e}\n"))
                     except ImportError as e:
                         if module_has_submodule(mod, module_name):
                             raise SiteListDataError(e)
